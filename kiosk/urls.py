b551e29a1a95d15ce28aefaa9f42c7d567470ed7
Ryan Chausse <chausse@gmail.com>
2021-10-27 13:40:29 -0700

    Initial setup views and urls

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from kiosk import views as kioskviews
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('kiosk/', include('kiosk.urls'), name='kiosk'),
-    path('', kioskviews.index, name='index'),
+    path('', views.index, name='index'),
 ]
--- kiosk/views.py
+++ kiosk/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Go to <a href='https://beta.yourvisionnow.com/kiosk'>Kiosk</a>")
+
+
+def kiosk(request):
+    return HttpResponse("Kiosk app - Your Vision Now")
