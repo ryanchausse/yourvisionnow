5c1e82824c999a55cc0f5546f7a7d1eb6a7f3905
Ryan Chausse <chausse@gmail.com>
2021-11-08 14:37:21 -0800

    Added: models for lenses, lense material, add-ons, and packages

--- kiosk/models.py
+++ kiosk/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class LensType(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField()
+    default_price = models.DecimalField()
+
+
+class LensMaterial(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField()
+    default_price = models.DecimalField()
+
+
+class LensAddOns(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField()
+    default_price = models.DecimalField()
+
+
+class LensPackage(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField()
+    lens_type = models.IntegerField(null=False)
+    lens_material = models.IntegerField(null=False)
+    lens_add_on = models.IntegerField(null=False)
+    promo_price = models.DecimalField()
+    retail_price = models.DecimalField()
