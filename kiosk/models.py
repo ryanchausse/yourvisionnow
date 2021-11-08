48bc16e27fd54b68de49dad8fe4a501688db3b89
Ryan Chausse <chausse@gmail.com>
2021-11-08 15:12:10 -0800

    Added: tables for customer and relational for customer and the lens package.

--- kiosk/models.py
+++ kiosk/models.py
@@ -2,28 +2,38 @@ from django.db import models
 
 
 class LensType(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField()
-    default_price = models.DecimalField()
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    default_price = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class LensMaterial(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField()
-    default_price = models.DecimalField()
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    default_price = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class LensAddOns(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField()
-    default_price = models.DecimalField()
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    default_price = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class LensPackage(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField()
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
     lens_type = models.IntegerField(null=False)
     lens_material = models.IntegerField(null=False)
     lens_add_on = models.IntegerField(null=False)
-    promo_price = models.DecimalField()
-    retail_price = models.DecimalField()
+    promo_price = models.DecimalField(max_digits=8, decimal_places=2)
+    retail_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class CustomerLensPackage(models.Model):
+    customer = models.IntegerField(null=False)
+    lens_package = models.IntegerField(null=False)
