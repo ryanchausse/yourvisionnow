d0ba27d8c72243907e94e2e22c8aa1ff4fa56bcd
Ryan Chausse <chausse@gmail.com>
2021-11-08 16:17:07 -0800

    Added: Customer and Lens and Package models to Django admin

--- kiosk/admin.py
+++ kiosk/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Customer
+from .models import LensType
+from .models import LensMaterial
+from .models import LensAddOns
+from .models import CustomerLensPackage
+from .models import LensPackage
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(LensType)
+admin.site.register(LensMaterial)
+admin.site.register(LensAddOns)
+admin.site.register(CustomerLensPackage)
+admin.site.register(LensPackage)
