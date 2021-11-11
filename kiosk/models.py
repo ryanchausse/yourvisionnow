bfb87adfcbbe26e322ea8278931ad0785ec049cb
Ryan Chausse <chausse@gmail.com>
2021-11-11 15:00:17 -0800

    Added: url img src field for lenstype for display on User selections card

--- kiosk/models.py
+++ kiosk/models.py
@@ -5,6 +5,7 @@ class LensType(models.Model):
     name = models.CharField(max_length=60)
     description = models.CharField(max_length=255)
     default_price = models.DecimalField(max_digits=8, decimal_places=2)
+    static_img_url = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
