02449c89d8cab16c038a95bc265ee8e3006e0a7c
Ryan Chausse <chausse@gmail.com>
2021-11-11 20:05:42 -0800

    Removed: exception for user with email chausse@gmail.com

--- yourvisionnow/adapters/override_default_login.py
+++ yourvisionnow/adapters/override_default_login.py
@@ -6,12 +6,9 @@ from django.forms import ValidationError
 
 class YourVisionNowSocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
-        if user_email(sociallogin.user) == 'chausse@gmail.com':
-            pass
-        else:
-            email_domain = user_email(sociallogin.user).split("@")[1]
-            if email_domain != 'gmail.com' or email_domain != 'yourvisionnow.com':
-                return HttpResponse('Please log in with a Google account that ends \
-                                    with "@gmail.com" or "@yourvisionnow.com". You \
-                                    may have to log out of your current Gmail account. \
-                                    Try again at <a href="./">Your Vision Now</a>')
+        email_domain = user_email(sociallogin.user).split("@")[1]
+        if email_domain != 'gmail.com' or email_domain != 'yourvisionnow.com':
+            return HttpResponse('Please log in with a Google account that ends \
+                                with "@gmail.com" or "@yourvisionnow.com". You \
+                                may have to log out of your current Gmail account. \
+                                Try again at <a href="./">Your Vision Now</a>')
