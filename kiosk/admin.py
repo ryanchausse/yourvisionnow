0893de6995ee0eec10706fc74a25c7a4c7460512
Ryan Chausse <chausse@gmail.com>
2021-11-17 15:27:07 -0800

    Added: registered new models in views

--- kiosk/admin.py
+++ kiosk/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Customer
+from .models import Order
 from .models import CustomerOrder
 from .models import LensType
 from .models import LensMaterial
 from .models import LensAddOns
 from .models import LensPackage
+from .models import LensPackageItem
 
 admin.site.register(Customer)
 admin.site.register(LensType)
@@ -12,3 +14,5 @@ admin.site.register(LensMaterial)
 admin.site.register(LensAddOns)
 admin.site.register(CustomerOrder)
 admin.site.register(LensPackage)
+admin.site.register(LensPackageItem)
+admin.site.register(Order)
